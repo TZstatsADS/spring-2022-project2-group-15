ee0b4480aace5f4be8bf028a9e3007511e199c66
fl2564 <97997450+fl2564@users.noreply.github.com>
2022-02-19 13:54:40 -0500

    Add files via upload
    this is the code for cleaning the 'permitted events in NYC'

---
+++ data/data cleaning.R
@@ -0,0 +1,33 @@
+library(dplyr)
+library(tidyverse)
+library(lubridate)
+
+##deal with data
+#load the data set
+data <- read_csv("/Users/fuchengliu/Desktop/project 2/NYC_Permitted_Event_Information.csv")
+
+#remove the repeating rows
+data2 <- data %>%
+  distinct()
+#remove the data with NA event id
+data_rm_na_id <- data2 %>%
+  filter(is.na(`Event ID`) == FALSE)
+
+
+#event borough
+borough_stat <- data_rm_na_id %>%
+  group_by(`Event Borough`) %>%
+  summarize(n = n())
+
+#see data with NA event type
+event_type_na <- data_rm_na_id %>%
+  filter(is.na(`Event Type`))
+
+#deal with the NA event
+e21124 <- unlist(strsplit(event_type_na$`Event Name`, split = "|", fixed = T))
+e21124 <- c(21124, e21124)
+data_use <- rbind(data_rm_na_id, e21124) %>%
+  filter(is.na(`Event Type`) == FALSE) %>%
+  arrange(`Event ID`)
+
+write.table(data_use,"data_use.csv",row.names=FALSE,col.names=TRUE,sep=",")
